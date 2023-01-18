5320fade5d6ec37560c4dcc0c78b0fd5898975e5
jeromanp <jeduardo.roman69@gmail.com>
2023-01-17 20:02:40 -0600

    server.js ya no se ocupa, se comenta codigo

--- Back/src/routes/server.js
+++ Back/src/routes/server.js
@@ -1,31 +1,38 @@
-const http = require("http");
-const PORT = 3001;
-// const characters = require("../utils/data");
-const { getCharById, getCharDetail } = require("../controllers/index");
+// // const http = require("http");
+// const PORT = 3001;
+// // const characters = require("../utils/data");
+// const { getCharById, getCharDetail } = require("../controllers/index");
 
-//funcion error
-function error(res) {
-  res.writeHead(404, { "Content-Type": "text/plain" });
-  res.end("Error!!, Route not found");
-}
+// const express= require ("express")
+// const app = express()
 
-http
-  .createServer((req, res) => {
-    const allUrl = req.url.split("/");
-    //devuelve un array, el cual el ID es la ultima posicion
-    // console.log(allUrl)
-    const characterID = Number(allUrl.pop()); //entrega el ID
-    const url = allUrl.join("/");
+// //funcion error
+// // function error(res) {
+// //   res.writeHead(404, { "Content-Type": "text/plain" });
+// //   res.end("Error!!, Route not found");
+// // }
+// function error(res){
+//   res.status(404).json("Error!!!, ROute not found")
+// }
 
-    switch (url) {
-      case "/onsearch":
-        return getCharById(res, characterID);
-      case "/detail":
-        return getCharDetail(res, characterID);
-      default:
-        error(res);
-    }
-  })
-  .listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`);
-  });
+// http
+//   .createServer((req, res) => {
+//     const allUrl = req.url.split("/");
+//     //devuelve un array, el cual el ID es la ultima posicion
+//     // console.log(allUrl)
+//     const characterID = Number(allUrl.pop()); //entrega el ID
+//     const url = allUrl.join("/");
+
+//     switch (url) {
+//       case "/onsearch":
+//         return getCharById(res, characterID);
+//       case "/detail":
+//         return getCharDetail(res, characterID);
+//       default:
+//         error(res);
+//     }
+//   })
+//   .listen(PORT, () => {
+//     console.log(`http://localhost:${PORT}`);
+//   });
+
