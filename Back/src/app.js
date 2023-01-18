978512114dc05bcf4046a17ce5c4c52025966180
jeromanp <jeduardo.roman69@gmail.com>
2023-01-17 19:44:12 -0600

    Incorporacion de Express

--- Back/src/app.js
+++ Back/src/app.js
@@ -1,12 +1,23 @@
-const http = require('http');
-const url = require('url');
-const db = require('./data');
+const express = requiere("express");
+const app = express();
+const bodyParser = require("body-parser");
+const routes = require("./routes");
 
-// console.log(db);
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
 
-const PORT = 3001;
-
-http.createServer((req, res) => {
-   const parsedUrl = url.parse(req.url, true);
-   console.log(parsedUrl);
+//Seteamos headers para la respuesta que le enviamos al cliente
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3001"); //Autorizo recibir solicitudes de este dominio
+  res.header("Access-Control-Allow-Credentials", true); //Autorizo recibir solicitudes que incluyan el encabezado con credenciales
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  ); //Autorizo recibir solicitudes con dichos hedears
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE"); //Autorizo las solicitudes tipo GET, POST, OPTIONS, PUT y DELETE.
+  next();
 });
+
+app.use("/", routes);
+
+module.exports = app;
