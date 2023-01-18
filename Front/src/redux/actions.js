bd2fbbcb2392f76b5e26b97f7b08c0b111f54dfa
jeromanp <jeduardo.roman69@gmail.com>
2023-01-17 20:14:42 -0600

    Cambio ruta de axios con server en express

--- Front/src/redux/actions.js
+++ Front/src/redux/actions.js
@@ -1,16 +1,29 @@
 import { ADD_FAV, DELETE_FAV, FILTER, ORDER, RESET } from "./types";
+import axios from "axios"
 
-export function addFavorite(character) {
-  return {
-    type: ADD_FAV,
-    payload: character,
+export function addFavorite(char) {
+  return function (dispatch) {
+    axios.post(`http://localhost:3001/rickandmorty/fav`, char)
+      .then((value) => value.json())
+      .then((dis) => {
+        dispatch({
+          type: ADD_FAV,
+          payload: dis,
+        });
+      });
   };
 }
 
 export function deleteFavorite(id) {
-  return {
-    type: DELETE_FAV,
-    payload: id,
+  return function (dispatch) {
+    axios.delete(`http://localhost:3001/rickandmorty/fav/${id}`)
+      .then((value) => value.data)
+      .then((dis) => {
+        dispatch({
+          type: DELETE_FAV,
+          payload: id,
+        });
+      });
   };
 }
 
