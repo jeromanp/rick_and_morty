1fc1180834cef0bf2e9264bdb16e30507c74f182
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-03-27 23:25:18 -0600

    console.log

--- Front/src/components/SeachBar/SearchBar.jsx
+++ Front/src/components/SeachBar/SearchBar.jsx
@@ -2,40 +2,37 @@ import styles from "./SearchBar.module.css";
 import { useState } from "react";
 
 export default function SearchBar(props) {
- 
-   const [characters, setCharacters] = useState("");
-   
-   function handleInputChange(event) {
-     setCharacters(event.target.value);
-   }
- 
-   // Función para borrar el valor del input
-   function handleClearInput() {
-     setCharacters("");
-   }
- 
-   return (
-     <div>
-       <input
-         className={styles.inputCard}
-         type="text"
-         placeholder="ID del personaje"   
-              
-         
-         onChange={(event) => handleInputChange(event)}
-         value={characters}
-         
-       />
-       
-       <button
-         className={styles.buscarCard}
-         onClick={() => {
-           props.onSearch(characters);
-           handleClearInput();
-         }}      
-       >
-         Add
-       </button>
-     </div>
-   );
- }
+  const [characters, setCharacters] = useState("");
+  //  console.log(characters);
+
+  function handleInputChange(event) {
+    setCharacters(event.target.value);
+  }
+
+  // Función para borrar el valor del input
+  function handleClearInput() {
+    setCharacters("");
+  }
+
+  return (
+    <div>
+      <input
+        className={styles.inputCard}
+        type="text"
+        placeholder="ID del personaje"
+        onChange={(event) => handleInputChange(event)}
+        value={characters}
+      />
+
+      <button
+        className={styles.buscarCard}
+        onClick={() => {
+          props.onSearch(characters);
+          handleClearInput();
+        }}
+      >
+        Add
+      </button>
+    </div>
+  );
+}
